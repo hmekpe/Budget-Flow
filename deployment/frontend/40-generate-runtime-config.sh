f7cdabe8a0a706e046f333d94215639c930cc72e
hmekpe <ekpehope19@gmail.com>
2026-03-30 20:01:48 +0000

    Split frontend nginx config for Render and compose

--- deployment/frontend/40-generate-runtime-config.sh
+++ deployment/frontend/40-generate-runtime-config.sh
@@ -3,5 +3,14 @@ set -eu
 
 template="/opt/budget-flow/runtime-config.js.template"
 target="/usr/share/nginx/html/runtime-config.js"
+standalone_conf="/opt/budget-flow/default.standalone.conf"
+compose_conf="/opt/budget-flow/default.compose.conf"
+nginx_conf="/etc/nginx/conf.d/default.conf"
+
+if [ "${ENABLE_INTERNAL_API_PROXY:-false}" = "true" ]; then
+  cp "$compose_conf" "$nginx_conf"
+else
+  cp "$standalone_conf" "$nginx_conf"
+fi
 
 envsubst '${AUTH_API_BASE_URL} ${FEATURE_API_BASE_URL}' < "$template" > "$target"
